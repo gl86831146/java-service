418bef510b4b1661480a280701299c85c0a9faf0
gao <2813314009@qq.com>
2025-04-21 15:52:55 +0800

    第一版

---
+++ src/test/java/top/gsc/javaservice/JavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.gsc.javaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
